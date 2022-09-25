3fa6d24d7548812a188afb50e8aa466722c08e74
Nicholas Vourlas <nicholasvourlas@gmail.com>
2022-09-24 22:13:39 -0400

    Finished questions 1 and 3

--- Questions1and3.m
+++ Questions1and3.m
@@ -10,7 +10,7 @@ finalTime=10;
 deltaTime=finalTime-initialTime;
 
 initialVelocity=0;
-finalVelocity=180/1000*3600;
+finalVelocity=180*1000/3600;
 deltaVelocity=finalVelocity-initialVelocity;
 
 distance=0;
@@ -19,7 +19,24 @@ averageAcceleration=0;
 
 %Calc and disp average acceleration
 averageAcceleration=deltaVelocity/deltaTime;
-disp(['b) ',num2str(averageAcceleration),'m/s^2'])
+disp(['b) ',num2str(averageAcceleration),'m/s^2']);
 
-distance=0.5*(initialVelocity+finalVelocity)*deltaTime;%Calc distance
-disp(['c) ',])
+%Calc and disp distance
+distance=0.5*(initialVelocity+finalVelocity)*deltaTime;
+disp(['c) ',num2str(distance),'m']);
+
+%Calc and disp final speed
+finalTime=12;
+deltaVelocity=(averageAcceleration*deltaTime)/1000*3600;
+disp(['d) ',num2str(deltaVelocity),'km/h']);
+
+disp(' ');
+disp('3)');
+
+% Define variables
+inputFereheit=100;
+outputCelcius=0;
+
+%Calc and disp temp in celcius
+outputCelcius=(inputFereheit-32)*5/9;
+disp(['a) ',num2str(outputCelcius),'*C']);
